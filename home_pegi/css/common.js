1b9fea49ae9f4aac91a1852d404dbe3986da0dd2
tetsu4746 <tetsu4746@icloud.com>
2021-05-27 10:50:42 +0900

    new

---
+++ home_pegi/css/common.js
@@ -0,0 +1,44 @@
+$(function() {
+   $('#html1').on('click',function(){
+      $('#html').fadeIn()
+     
+   });
+ 
+ });
+ $(function() {
+  $('#jq1').on('click',function(){
+     $('#jq').fadeIn()
+    
+  });
+
+});$(function() {
+  $('#css1').on('click',function(){
+     $('#css').fadeIn()
+    
+  });
+
+});$(function() {
+  $('#js1').on('click',function(){
+     $('#js').fadeIn()
+    
+  });
+
+});
+ 
+ // メニューをクリックされたら、非表示に
+
+
+// $(function() {
+//  $('#html1').click (function(){
+//    $("#html").toggle(1000);
+   
+//  });
+
+// });
+// $(function() {
+//  $('#btn2').click (function(){
+//    $("#img1").toggle(1000);
+   
+//  });
+
+
